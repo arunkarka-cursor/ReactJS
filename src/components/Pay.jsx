e8448b1da9e14b327cce4dfa253cb4405c3db82f
arunkarka <arunkarka@gmail.com>
2025-12-21 19:46:54 +0000

    First Commit

---
+++ src/components/Pay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Pay.css';
+
+const Pay = () => {
+    return (
+        <div>
+            <h1>Pay Component</h1>
+        </div>
+    );
+};
+
+export default Pay;
